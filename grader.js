1fd8497f2cfbd1bdb2c2a0c84904093015bd067f
Ubuntu <ubuntu@ip-10-144-4-231.ec2.internal>
2013-07-09 11:39:47 +0000

    added restlter library and call to fetch url

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,12 +56,24 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var chkString = function(s, checksfile) {
+    $ = cheerio.load(s);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+/*
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
@@ -70,6 +83,29 @@ if(require.main == module) {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
+*/
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url for html file')
+        .parse(process.argv);
+    if(program.url) {
+        rest.get(program.url).on('complete', function(urlcontents) {
+            var checkJson = chkString(urlcontents, program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson); 
+        });
+    }
+    else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
